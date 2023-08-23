94e3a63bd7c895fde159e987a72950f816dd271a
kyaakpa <kyaakpa@users.noreply.github.com>
2023-08-23 20:30:15 +1000

    challenge completed

--- index.js
+++ index.js
@@ -24,6 +24,15 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+app.get('/api/:cmd', (req,res)=> {
+  const ip = req.ip
+  const lang = req.headers['accept-language']
+  const os = req.headers['user-agent']
+  res.json({ipaddress: ip, language: lang, software: os})
+})
+
+
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
